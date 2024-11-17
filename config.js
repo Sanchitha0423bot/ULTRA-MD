ce8d564e2780d08b3abe2397ac59f5f7dc8f212a
Sanchitha0423bot <ffirejiro@gmail.com>
2024-11-17 13:42:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '94702722766';
+const defaultOwner = '94742817784';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
@@ -20,7 +20,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'ULTRA-MD';
-global.botNumber = '94702722766' //replace your bot number here
+global.botNumber = '94742817784' //replace your bot number here
 
 global.mods = []
 global.prems = []
